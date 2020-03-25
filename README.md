81e2da9008e228f2f2058bb4208ffdc2f762d00d
unknown <gorazdaaleksander@gmail.com>
2020-03-25 14:45:32 +0100

    AG 2020-03-25

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
-# CeneoScraper
+# CeneoScraper11S
 # Etap 1 - pobranie pojedynczeej opinii 
-- opinia: li.review-box
-- identyfikator: li.review-box["data-entry-id"]
-- autor: div.reviewer-name-line
-- rekomendacja: div.product-review-summary > em
-- liczba gwiazdek: span.review-score-count
-- czy potwierdzona zakupem: div.product-review-pz
-- data wystawienia: span.review-time > time["datetime"][0]
-- data zakupu: span.review-time > time["datetime"][1]
-- przydatna: button.votes-yes["data-total-vote"]
-- nieprzydatna: button.votes-no["data-total-vote"]
-- treść: p.product-review-body
-- wady: div.cons-cell > ul
-- zalety: div.pros-cell > ul
-## Etap 2 - pobranie wszystkich opinii z pojedynczej strony
+- opinia    : li.review-box
+- identyfikator : li.review-box[data-entry-id]
+- autor     : div.reviewer-name-line
+- rekomendacja      : div.product-review-summary > em
+- liczba gwiazdek       :  span.review-score-count
+- czy potwierdzona zakupem  :  div.product-review-pz
+- data wystawienia      : span.review-time > time  (pierwsze wystapienie)
+- data zakupu       :  span.review-time > time  (drugie wystapienie)
+- przydatna     : button.votes-yes[data-total-vote]
+- nieprzydatna      : button.votes-no[data-total-vote]
+- treść     : p.product-review-body
+- wady      : div.pros-cell > ul
+- zalety    : div.cons-cell > ul
+
+04.03.2020
+
+## Etap 2 - pobiarnie wszystkich opinii z pojedynczej strony
 - zapis składowych opinii do złożonej struktury danych
-## Etap 3 - pobranie wszystkich opinii o pojedynczym produkcie
-- sposób przechodzenia po kolejnych stronqach z opiniami 
-- eksport opinii do pliku (.csv lub .xlsx lub .json)
+## Etap 3 - Pobranie wszystkich opinii o pojedynczym produkcie
+- sposób przechodzenia po poszczegolnych stronach z opiniami
+- Eksport opinii do pliku .js
--- scrapper.py
+++ scrapper.py
@@ -1,55 +1,82 @@
 #import bibliotek
 import requests
 from bs4 import BeautifulSoup
-#adres URl strony z opiniami
-url = "https://www.ceneo.pl/81643787#tab=reviews"
+import pprint
+import json
 
-#pobranie kodu HTML strony z adresu URL
-page_response = requests.get(url)
-page_tree = BeautifulSoup(page_response.text, 'html.parser')
+#adres URL strony z opiniami
+url_prefix = "https://www.ceneo.pl"
+product_id = input("Podaj kod produktu: ")
+url_postfix = "#tab=reviews"
+url = url_prefix+"/"+product_id+url_postfix
 
-#wybranie z kodu strony fragmentów odpowiadających poszczególnym opiniom
-opinions = page_tree.select("li.review-box")
-opinion = opinions.pop()
-opinion_id = opinion["data-entry-id"]
-author = opinion.select('div.reviewer-name-line').pop().string
-try:
-    recomendation = opinion.select('div.product-review-summary > em').pop().string
-except IndexError:
-    recomendation = None
-stars = opinion.select('span.review-score-count').pop().string
-try:
-    purchased = opinion.select('div.product-review-pz').pop().string
-except IndexError:
-    purchased = None
-useful = opinion.select('button.vote-yes').pop()["data-total-vote"]
-useless = opinion.select('button.vote-no').pop()["data-total-vote"]
-content = opinion.select('p.product-review-body').pop().get_text()
-# - opinia: li.review-box
-# - identyfikator: li.review-box["data-entry-id"]
-# - autor: div.reviewer-name-line
-# - rekomendacja: div.product-review-summary > em
-# - liczba gwiazdek: span.review-score-count
-# - czy potwierdzona zakupem: div.product-review-pz
-# - data wystawienia: span.review-time > time["datetime"][0]
-# - data zakupu: span.review-time > time["datetime"][1]
-# - przydatna: button.votes-yes["data-total-vote"]
-# - nieprzydatna: button.votes-no["data-total-vote"]
-# - treść: p.product-review-body
-# - wady: div.cons-cell > ul
-try:
-    cons = opinion.select('div.cons-cell > ul').pop().get_text()
-except IndexError:
-    cons = None
-try:
-    pros = opinion.select('div.pros-cell > ul').pop().get_text()
-except IndexError:
-    pros = None 
-date = opinion.select('span.review.time > time')
-review_date = date.pop(0)["datetime"]
-try:
-    purchase_date = date.pop(0)["datetime"]
-except IndexError:
-    purchase_date = None
-# - zalety: div.pros-cell > ul
-#ekstrakcja składowyh dla pierwszej opinii z listy
+#pusta lista na opinie
+opinions_list = []
+
+while url is not None:
+    #pobranie kodu HTML strony z adresu URL
+    page_response = requests.get(url)
+    page_tree = BeautifulSoup(page_response.text, 'html.parser')
+
+    #wybranie z kodu strony fragmentów odpowiadających poszczególnym opiniom
+    opinions = page_tree.select("li.js_product-review")
+    
+    #ekstrakcja składowych dla pojedynczej opinii z listy
+    for opinion in opinions: 
+        opinion_id = opinion["data-entry-id"]
+        author = opinion.select('div.reviewer-name-line').pop().string.strip()
+        try:
+            recomendation = opinion.select('div.product-review-summary > em').pop().string.strip()
+        except IndexError:
+            recomendation = None
+        stars = opinion.select('span.review-score-count').pop().string.strip()
+        try:
+            purchased = opinion.select('div.product-review-pz').pop().string
+        except IndexError:
+            purchased = None   
+        useful = opinion.select('button.vote-yes').pop()["data-total-vote"]
+        useless = opinion.select('button.vote-no').pop()["data-total-vote"]
+        content = opinion.select('p.product-review-body').pop().get_text().strip()
+        try:
+            cons = opinion.select('div.cons-cell > ul').pop().get_text().strip()
+        except IndexError:
+            cons = None
+        try:
+            pros = opinion.select('div.pros-cell > ul').pop().get_text().strip()
+        except IndexError:
+            pros = None
+        date = opinion.select('span.review-time > time')
+        review_date = date.pop(0)["datetime"]
+        try:
+            purchase_date = date.pop(0)["datetime"]
+        except IndexError:
+            purchase_date = None
+        
+        opinion_dict = {
+            "opinion_id":opinion_id,
+            "author":author,
+            "recomendation":recomendation,
+            "stars":stars,
+            "content": content,
+            "pros": pros,
+            "cons":cons, 
+            "useful":useful,
+            "useless":useless,
+            "purchased":purchased,
+            "purchase_date":purchase_date,
+            "review_date":review_date
+        }
+        opinions_list.append(opinion_dict)
+
+    try:
+        url = url_prefix+page_tree.select("a.pagination__next").pop()["href"]
+    except IndexError:
+        url = None
+
+    print("url:",url)
+
+with open(product_id+".json", 'w', encoding='utf-8') as fp:
+    json.dump(opinions_list, fp, ensure_ascii=False, indent=4, )
+
+
+
